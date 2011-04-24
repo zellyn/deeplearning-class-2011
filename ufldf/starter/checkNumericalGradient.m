f86f5c0d62ee3b72bd773f31970085835a813a69
Zellyn Hunter <zellyn@gmail.com>
2011-04-24 16:25:29 -0700

    Added checkNumericalGradient, implemented computeNumericalGradient

---
+++ ufldf/starter/checkNumericalGradient.m
@@ -0,0 +1,25 @@
+function checkNumericalGradient()
+
+function y = simple_func(x)
+  y = x(1)^2 + 3 * x(1) * x(2);
+endfunction
+
+function dy = simple_grad(x)
+  dy = [2 * x(1) + 3 * x(2); 3 * x(1)];
+endfunction
+
+for i = 1:100
+  x = rand([2,1]) * 10 - 5;
+  grad = simple_grad(x);
+  numgrad = computeNumericalGradient(@simple_func, x);
+  diff = norm(numgrad-grad)/norm(numgrad+grad);
+  if (diff > 1e-4)
+    disp('Problem with numerical gradient!')
+    disp([x numgrad grad]);
+  end
+end
+
+% Use this to eyeball the gradients
+disp([numgrad grad]);
+
+endfunction
--- ufldf/starter/computeNumericalGradient.m
+++ ufldf/starter/computeNumericalGradient.m
@@ -3,25 +3,26 @@ function numgrad = computeNumericalGradient(J, theta)
 % theta: is a vector of parameters
 % J:  is a scalar real-valued function; calling J(theta) will return the
 % function value at theta
-  
+
 % Initialize numgrad with zeros
 numgrad = zeros(size(theta));
+
+epsilon = 1e-4;
 
 %% ---------- YOUR CODE HERE --------------------------------------
 %  Instructions: Implement numerical gradient checking for your code
 %                to compute numgrad (gradient from finite differences)
 %                (Check Section 2.3 in the lecture notes)
-%                
+%
 % Hint: You may want to compute each element of numgrad at a time.
 
-
-
-
-
-
-
-
-
+for i = 1:numel(theta)
+  theta_minus = theta;
+  theta_plus = theta;
+  theta_minus(i) = theta_minus(i) - epsilon;
+  theta_plus(i) = theta_plus(i) + epsilon;
+  numgrad(i) = (J(theta_plus) - J(theta_minus)) / (2*epsilon);
+end
 
 %% ---------------------------------------------------------------
 end

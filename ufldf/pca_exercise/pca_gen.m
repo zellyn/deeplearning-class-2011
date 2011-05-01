6016231562683809a435088389cb93bc8aa46b6e
Zellyn Hunter <zellyn@gmail.com>
2011-05-01 16:55:20 -0700

    implemented pca exercise

--- ufldf/pca_exercise/pca_gen.m
+++ ufldf/pca_exercise/pca_gen.m
@@ -11,10 +11,11 @@ randsel = randi(size(x,2),200,1); % A random selection of samples for visualizat
 display_network(x(:,randsel));
 
 %%================================================================
-%% Step 0b: Zero-mean the data (by row)
+%% Step 0b: Zero-mean the data (by column)
 %  You can make use of the mean and repmat/bsxfun functions.
 
-% -------------------- YOUR CODE HERE -------------------- 
+avg = mean(x, 1);
+x = x - repmat(avg, size(x, 1), 1);
 
 %%================================================================
 %% Step 1a: Implement PCA to obtain xRot
@@ -22,8 +23,10 @@ display_network(x(:,randsel));
 %  with respect to the eigenbasis of sigma, which is the matrix U.
 
 
-% -------------------- YOUR CODE HERE -------------------- 
-xRot = zeros(size(x)); % You need to compute this
+% -------------------- YOUR CODE HERE --------------------
+sigma = x * x' / size(x, 2);
+[U, S, V] = svd(sigma);
+xRot = U' * x;
 
 
 %%================================================================
@@ -31,12 +34,11 @@ xRot = zeros(size(x)); % You need to compute this
 %  The covariance matrix for the data expressed with respect to the basis U
 %  should be a diagonal matrix with non-zero entries only along the main
 %  diagonal. We will verify this here.
-%  Write code to compute the covariance matrix, covar. 
+%  Write code to compute the covariance matrix, covar.
 %  When visualised as an image, you should see a straight line across the
 %  diagonal (non-zero entries) against a blue background (zero entries).
 
-% -------------------- YOUR CODE HERE -------------------- 
-covar = zeros(size(x, 1)); % You need to compute this
+covar = xRot * xRot' / size(xRot, 2);
 
 % Visualise the covariance matrix. You should see a line across the
 % diagonal against a blue background.
@@ -48,9 +50,8 @@ imagesc(covar);
 %  Write code to determine k, the number of components to retain in order
 %  to retain at least 99% of the variance.
 
-% -------------------- YOUR CODE HERE -------------------- 
-k = 0; % Set k accordingly
-
+SD = diag(S);
+k = length(SD((cumsum(SD) / sum(SD)) <= 0.99));
 
 %%================================================================
 %% Step 3: Implement PCA with dimension reduction
@@ -59,16 +60,15 @@ k = 0; % Set k accordingly
 %  data in k dimensions instead of the original 144, which will save you
 %  computational time when running learning algorithms on the reduced
 %  representation.
-% 
-%  Following the dimension reduction, invert the PCA transformation to produce 
+%
+%  Following the dimension reduction, invert the PCA transformation to produce
 %  the matrix xHat, the dimension-reduced data with respect to the original basis.
 %  Visualise the data and compare it to the raw data. You will observe that
 %  there is little loss due to throwing away the principal components that
 %  correspond to dimensions with low variation.
 
-% -------------------- YOUR CODE HERE -------------------- 
-xHat = zeros(size(x));  % You need to compute this
-
+xRot = U(:,1:k)' * x;
+xHat = U(:,1:k) * xRot;
 
 % Visualise the data, and compare it to the raw data
 % You should observe that the raw and processed data are of comparable quality.
@@ -83,30 +83,28 @@ display_network(x(:,randsel));
 %%================================================================
 %% Step 4a: Implement PCA with whitening and regularisation
 %  Implement PCA with whitening and regularisation to produce the matrix
-%  xPCAWhite. 
+%  xPCAWhite.
 
 epsilon = 0.1;
-xPCAWhite = zeros(size(x));
-
-% -------------------- YOUR CODE HERE -------------------- 
+xPCAWhite = diag(1./sqrt(diag(S) + epsilon)) * U' * x;
 
 %%================================================================
-%% Step 4b: Check your implementation of PCA whitening 
-%  Check your implementation of PCA whitening with and without regularisation. 
-%  PCA whitening without regularisation results a covariance matrix 
+%% Step 4b: Check your implementation of PCA whitening
+%  Check your implementation of PCA whitening with and without regularisation.
+%  PCA whitening without regularisation results a covariance matrix
 %  that is equal to the identity matrix. PCA whitening with regularisation
-%  results in a covariance matrix with diagonal entries starting close to 
+%  results in a covariance matrix with diagonal entries starting close to
 %  1 and gradually becoming smaller. We will verify these properties here.
-%  Write code to compute the covariance matrix, covar. 
+%  Write code to compute the covariance matrix, covar.
 %
-%  Without regularisation (set epsilon to 0 or close to 0), 
+%  Without regularisation (set epsilon to 0 or close to 0),
 %  when visualised as an image, you should see a red line across the
 %  diagonal (one entries) against a blue background (zero entries).
 %  With regularisation, you should see a red line that slowly turns
 %  blue across the diagonal, corresponding to the one entries slowly
 %  becoming smaller.
 
-% -------------------- YOUR CODE HERE -------------------- 
+covar = xPCAWhite * xPCAWhite' / size(xPCAWhite, 2);
 
 % Visualise the covariance matrix. You should see a red line across the
 % diagonal against a blue background.
@@ -115,13 +113,11 @@ imagesc(covar);
 
 %%================================================================
 %% Step 5: Implement ZCA whitening
-%  Now implement ZCA whitening to produce the matrix xZCAWhite. 
+%  Now implement ZCA whitening to produce the matrix xZCAWhite.
 %  Visualise the data and compare it to the raw data. You should observe
 %  that whitening results in, among other things, enhanced edges.
 
-xZCAWhite = zeros(size(x));
-
-% -------------------- YOUR CODE HERE -------------------- 
+xZCAWhite = U * xPCAWhite;
 
 % Visualise the data, and compare it to the raw data.
 % You should observe that the whitened images have enhanced edges.

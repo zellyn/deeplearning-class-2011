a3897101ac05478d5438a31c504dc9c74f28b9d3
Zellyn Hunter <zellyn@gmail.com>
2011-05-07 13:17:44 -0700

    nascent librarization

---
+++ ufldf/library/imports.py
@@ -0,0 +1,11 @@
+import time
+import sys
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+if sys.platform == 'darwin': mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+from scipy import linalg
+from mpl_toolkits.mplot3d import Axes3D
+import scipy.io as sio
+import scipy.optimize

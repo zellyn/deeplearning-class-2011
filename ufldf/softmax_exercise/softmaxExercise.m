e13ac577b4cda289844894cbf866c0f2137d4ea4
Zellyn Hunter <zellyn@gmail.com>
2011-05-07 18:00:24 -0700

    minor tweaks, use most-varying data for debug

--- ufldf/softmax_exercise/softmaxExercise.m
+++ ufldf/softmax_exercise/softmaxExercise.m
@@ -1,20 +1,22 @@
-%% CS294A/CS294W Softmax Exercise 
+%% CS294A/CS294W Softmax Exercise
 
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the
-%  softmax exercise. You will need to write the softmax cost function 
-%  in softmaxCost.m and the softmax prediction function in softmaxPred.m. 
+%  softmax exercise. You will need to write the softmax cost function
+%  in softmaxCost.m and the softmax prediction function in softmaxPred.m.
 %  For this exercise, you will not need to change any code in this file,
 %  or any other files other than those mentioned above.
 %  (However, you may be required to do so in later exercises)
+
+addpath '../library/'
 
 %%======================================================================
 %% STEP 0: Initialise constants and parameters
 %
 %  Here we define and initialise some constants which allow your code
-%  to be used more generally on any arbitrary input. 
+%  to be used more generally on any arbitrary input.
 %  We also initialise some parameters used for tuning the model.
 
 inputSize = 28 * 28; % Size of input vector (MNIST images are 28x28)
@@ -26,27 +28,32 @@ lambda = 1e-3; % Weight decay parameter
 %% STEP 1: Load data
 %
 %  In this section, we load the input and output data.
-%  For softmax regression on MNIST pixels, 
-%  the input data is the images, and 
+%  For softmax regression on MNIST pixels,
+%  the input data is the images, and
 %  the output data is the labels.
 %
 
-% Change the location of the files to the directory where you 
+% Change the location of the files to the directory where you
 % have saved them
-images = loadMNISTImages('mnist/train-images-idx3-ubyte');
-labels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
+images = loadMNISTImages('../data/train-images-idx3-ubyte');
+labels = loadMNISTLabels('../data/train-labels-idx1-ubyte');
 labels(labels==0) = 10; % Remap 0 to 10
 
 inputData = images;
 
 % For debugging purposes, you may wish to reduce the size of the input data
-% in order to speed up gradient checking. 
+% in order to speed up gradient checking.
 % Here, we consider only 8 pixels of the images, and only the first 100
 % images.
 DEBUG = true; % Set DEBUG to true when debugging.
 if DEBUG
     inputSize = 8;
-    inputData = inputData(1:8, 1:100);
+    % only 100 datapoints
+    inputData = inputData(:, 1:100);
+    [_, i] = sort(var(inputData, 0, 2), "descend");
+    indices = i(1:inputSize);
+    % only top inputSize most-varying input elements (pixels)
+    inputData = inputData(indices, :);
     labels = labels(1:100);
 end
 
@@ -57,27 +64,27 @@ theta = sqrt(6) / sqrt(numClasses * inputSize) ...
 %%======================================================================
 %% STEP 2: Implement softmaxCost
 %
-%  Implement softmaxCost in softmaxCost.m. 
+%  Implement softmaxCost in softmaxCost.m.
 
 [cost, grad] = softmaxCost(theta, numClasses, inputSize, lambda, inputData, labels);
-                                     
+
 %%======================================================================
 %% STEP 3: Gradient checking
 %
 %  As with any learning algorithm, you should always check that your
 %  gradients are correct before learning the parameters.
-% 
+%
 
 numGrad = computeNumericalGradient( @(x) softmaxCost(x, numClasses, ...
                                     inputSize, lambda, inputData, labels), theta);
 
 % Use this to visually compare the gradients side by side
-disp([numGrad grad]); 
+disp([numGrad grad]);
 
 % Compare numerically computed gradients with those computed analytically
 diff = norm(numGrad-grad)/norm(numGrad+grad);
-disp(diff); 
-% The difference should be small. 
+disp(diff);
+% The difference should be small.
 % In our implementation, these values are usually less than 1e-7.
 
 % When your gradients are correct, congratulations!
@@ -85,15 +92,15 @@ disp(diff);
 %%======================================================================
 %% STEP 4: Learning parameters
 %
-%  Once you have verified that your gradients are correct, 
+%  Once you have verified that your gradients are correct,
 %  you can start training your softmax regression code using softmaxTrain
 %  (which uses minFunc).
 
 options.maxIter = 100;
 softmaxModel = softmaxTrain(inputSize, numClasses, lambda, ...
                             inputData, labels, options);
-                          
-% Although we only use 100 iterations here to train a classifier for the 
+
+% Although we only use 100 iterations here to train a classifier for the
 % MNIST data set, in practice, training for more iterations is usually
 % beneficial.
 
@@ -123,7 +130,7 @@ fprintf('Accuracy: %0.3f%%\n', acc * 100);
 %
 % Accuracy: 92.200%
 %
-% If your values are too low (accuracy less than 0.91), you should check 
-% your code for errors, and make sure you are training on the 
-% entire data set of 60000 28x28 training images 
+% If your values are too low (accuracy less than 0.91), you should check
+% your code for errors, and make sure you are training on the
+% entire data set of 60000 28x28 training images
 % (unless you modified the loading code, this should be the case)

4186bdde8671fe2ab8ad14c8479eafa1ded62ddc
Zellyn Hunter <zellyn@gmail.com>
2011-05-19 08:22:34 -0700

    implemented stacked predict

--- ufldf/stackedae_exercise/stackedAEPredict.m
+++ ufldf/stackedae_exercise/stackedAEPredict.m
@@ -1,17 +1,17 @@
 function [pred] = stackedAEPredict(theta, inputSize, hiddenSize, numClasses, netconfig, data)
-                                         
+
 % stackedAEPredict: Takes a trained theta and a test data set,
 % and returns the predicted labels for each example.
-                                         
+
 % theta: trained weights from the autoencoder
 % visibleSize: the number of input units
 % hiddenSize:  the number of hidden units *at the 2nd layer*
 % numClasses:  the number of categories
-% data: Our matrix containing the training data as columns.  So, data(:,i) is the i-th training example. 
+% data: Our matrix containing the training data as columns.  So, data(:,i) is the i-th training example.
 
-% Your code should produce the prediction matrix 
+% Your code should produce the prediction matrix
 % pred, where pred(i) is argmax_c P(y(c) | x(i)).
- 
+
 %% Unroll theta parameter
 
 % We first extract the part which compute the softmax gradient
@@ -21,18 +21,23 @@ softmaxTheta = reshape(theta(1:hiddenSize*numClasses), numClasses, hiddenSize);
 stack = params2stack(theta(hiddenSize*numClasses+1:end), netconfig);
 
 %% ---------- YOUR CODE HERE --------------------------------------
-%  Instructions: Compute pred using theta assuming that the labels start 
+%  Instructions: Compute pred using theta assuming that the labels start
 %                from 1.
 
+numCases = size(data, 2);
+groundTruth = full(sparse(labels, 1:numCases, 1));
 
+depth = numel(stack);
+z = cell(depth+1,1);
+a = cell(depth+1, 1);
+a{1} = data;
 
+for layer = (1:depth)
+  z{layer+1} = stack{layer}.w * a{layer} + repmat(stack{layer}.b, [1, size(a{layer},2)]);
+  a{layer+1} = sigmoid(z{layer+1});
+end
 
-
-
-
-
-
-
+[~, pred] = max(softmaxTheta * a{depth+1});
 
 % -----------------------------------------------------------
 

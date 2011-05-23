fb9785bf1589e9fe683bc53d3932b31e7e2a0a33
Zellyn Hunter <zellyn@gmail.com>
2011-05-22 18:30:44 -0700

    added cnn starter code

---
+++ ufldf/cnn_exercise/cnnConvolve.m
@@ -0,0 +1,41 @@
+function convolvedFeatures = cnnConvolve(patchDim, numFeatures, images, W, b, ZCAWhite, meanPatch)
+%cnnConvolve Returns the convolution of the features given by W and b with
+%the given images
+%
+% Parameters:
+%  patchDim - patch (feature) dimension
+%  numFeatures - number of features
+%  images - large images to convolve with, matrix in the form
+%           images(r, c, channel, image number)
+%  W, b - W, b for features from the sparse autoencoder
+%  ZCAWhite, meanPatch - ZCAWhitening and meanPatch matrices used for
+%                        preprocessing
+%
+% Returns:
+%  convolvedFeatures - matrix of convolved features in the form
+%                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
+
+numImages = size(images, 4);
+imageDim = size(images, 1);
+imageChannels = size(images, 3);
+
+convolvedFeatures = zeros(numFeatures, numImages, imageDim - patchDim + 1, imageDim - patchDim + 1);
+
+% -------------------- YOUR CODE HERE --------------------
+% Instructions:
+%   Convolve every feature with every large image here to produce the 
+%   numFeatures x numImages x (imageDim - patchDim + 1) x (imageDim - patchDim + 1) 
+%   matrix convolvedFeatures, such that 
+%   convolvedFeatures(featureNum, imageNum, imageRow, imageCol) is the
+%   value of the convolved featureNum feature for the imageNum image over
+%   the region (imageRow, imageCol) to (imageRow + patchDim - 1, imageCol + patchDim - 1)
+%
+% Expected running times: 
+%   Convolving with 100 images should take less than 3 minutes 
+%   Convolving with 5000 images should take around an hour
+%   (So to save time when testing, you should convolve with less images, as
+%   described earlier)
+% -------------------- YOUR CODE HERE --------------------
+
+end
+

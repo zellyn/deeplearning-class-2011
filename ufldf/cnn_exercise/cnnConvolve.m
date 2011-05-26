cc9d75d9a6c2b249d71dc33be457fb78694d7c3a
Zellyn Hunter <zellyn@gmail.com>
2011-05-25 23:02:42 -0700

    debugging cnn

--- ufldf/cnn_exercise/cnnConvolve.m
+++ ufldf/cnn_exercise/cnnConvolve.m
@@ -15,22 +15,26 @@ function convolvedFeatures = cnnConvolve(patchDim, numFeatures, images, W, b, ZC
 %  convolvedFeatures - matrix of convolved features in the form
 %                      convolvedFeatures(featureNum, imageNum, imageRow, imageCol)
 
+patchSize = patchDim * patchDim;
 numImages = size(images, 4);
 imageDim = size(images, 1);
 imageChannels = size(images, 3);
 
+assert(numFeatures == size(W, 1), 'W should have numFeatures rows');
+assert(patchSize*imageChannels == size(W, 2), 'W should have patchDim^2*imageChannels columns');
+
 convolvedFeatures = zeros(numFeatures, numImages, imageDim - patchDim + 1, imageDim - patchDim + 1);
 
 % Instructions:
-%   Convolve every feature with every large image here to produce the 
-%   numFeatures x numImages x (imageDim - patchDim + 1) x (imageDim - patchDim + 1) 
-%   matrix convolvedFeatures, such that 
+%   Convolve every feature with every large image here to produce the
+%   numFeatures x numImages x (imageDim - patchDim + 1) x (imageDim - patchDim + 1)
+%   matrix convolvedFeatures, such that
 %   convolvedFeatures(featureNum, imageNum, imageRow, imageCol) is the
 %   value of the convolved featureNum feature for the imageNum image over
 %   the region (imageRow, imageCol) to (imageRow + patchDim - 1, imageCol + patchDim - 1)
 %
-% Expected running times: 
-%   Convolving with 100 images should take less than 3 minutes 
+% Expected running times:
+%   Convolving with 100 images should take less than 3 minutes
 %   Convolving with 5000 images should take around an hour
 %   (So to save time when testing, you should convolve with less images, as
 %   described earlier)
@@ -40,10 +44,26 @@ convolvedFeatures = zeros(numFeatures, numImages, imageDim - patchDim + 1, image
 % that you need to take into account the whitening and mean subtraction
 % steps
 
+% size(b)  % 400 x 1
+% size(W)  % 400 x 192
+% size(ZCAWhite)  % 192 x 192
+% size(meanPatch)  % 192 x 1
+% size(images)  % 64 x 64 x 3 x 8
 
+% size(WT) = 400 x 192
+% size(x) = 192 * m
+% size(b_w) = 400 x 1
 
+WT = W * ZCAWhite;
+b_w = b - WT * meanPatch;
 
-
+% size(W) == size(WT) == (numFeatures, patchDim * imageChannels)
+% Compute per-channel features for 2-D convolution
+features = zeros(imageChannels, numFeatures, patchSize);
+for channel = 1:imageChannels
+  offset = patchSize * (channel-1);
+  features(channel, :, :) = WT(:, offset+1:offset+patchSize);
+end
 
 % --------------------------------------------------------
 
@@ -53,48 +73,63 @@ for imageNum = 1:numImages
 
     % convolution of image with feature matrix for each channel
     convolvedImage = zeros(imageDim - patchDim + 1, imageDim - patchDim + 1);
-    for channel = 1:3
+    for channel = 1:imageChannels
 
       % Obtain the feature (patchDim x patchDim) needed during the convolution
       % ---- YOUR CODE HERE ----
+      offset = patchSize * (channel-1);
+      feature = WT(featureNum, offset+1:offset+patchSize);
 
-      
-      
-      
-      
+      if (featureNum == 26) && (imageNum == 2)
+        patch = images(13:13 + patchDim - 1, 53:53 + patchDim - 1, channel, imageNum);
+        patch = patch(:);
+
+        % size(feature)
+        % size(patch)
+        % sum(feature)
+        feature * patch
+      end
+
+      feature = reshape(feature, patchDim, patchDim);
+
       % ------------------------
 
       % Flip the feature matrix because of the definition of convolution, as explained later
       feature = flipud(fliplr(squeeze(feature)));
-      
+
       % Obtain the image
       im = squeeze(images(:, :, channel, imageNum));
+      im = flipud(fliplr(im));
 
       % Convolve "feature" with "im", adding the result to convolvedImage
       % be sure to do a 'valid' convolution
       % ---- YOUR CODE HERE ----
 
-      
-      
-      
+      convolved = conv2(im, feature, 'valid');
+      if (featureNum == 26) && (imageNum == 2)
+        convolved(13, 53)
+      end
+      convolvedImage = convolvedImage + convolved;
+
       % ------------------------
 
     end
-    
+
     % Subtract the bias unit (correcting for the mean subtraction as well)
     % Then, apply the sigmoid function to get the hidden activation
     % ---- YOUR CODE HERE ----
 
-    
-    
-    
+    convolvedImage = sigmoid(convolvedImage + b_w(featureNum));
+
     % ------------------------
-    
+
     % The convolved feature is the sum of the convolved values for all channels
     convolvedFeatures(featureNum, imageNum, :, :) = convolvedImage;
   end
 end
 
-
 end
 
+function sigm = sigmoid(x)
+    sigm = 1 ./ (1 + exp(-x));
+end
--- ufldf/cnn_exercise/cnnExercise.m
+++ ufldf/cnn_exercise/cnnExercise.m
@@ -2,11 +2,13 @@
 
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the
 %  convolutional neural networks exercise. In this exercise, you will only
 %  need to modify cnnConvolve.m and cnnPool.m. You will not need to modify
 %  this file.
+
+addpath '../library/'
 
 %%======================================================================
 %% STEP 0: Initialization
@@ -18,30 +20,29 @@ imageChannels = 3;     % number of channels (rgb, so 3)
 patchDim = 8;          % patch dimension
 numPatches = 50000;    % number of patches
 
-visibleSize = patchDim * patchDim * imageChannels;  % number of input units 
+visibleSize = patchDim * patchDim * imageChannels;  % number of input units
 outputSize = visibleSize;   % number of output units
-hiddenSize = 400;           % number of hidden units 
+hiddenSize = 400;           % number of hidden units
 
 epsilon = 0.1;	       % epsilon for ZCA whitening
 
 poolDim = 16;          % dimension of pooling region
 
 %%======================================================================
-%% STEP 1: Train a sparse autoencoder (with a linear decoder) to learn 
+%% STEP 1: Train a sparse autoencoder (with a linear decoder) to learn
 %  features from color patches. If you have completed the linear decoder
-%  execise, use the features that you have obtained from that exercise, 
-%  loading them into optTheta. Recall that we have to keep around the 
+%  execise, use the features that you have obtained from that exercise,
+%  loading them into optTheta. Recall that we have to keep around the
 %  parameters used in whitening (i.e., the ZCA whitening matrix and the
 %  meanPatch)
 
 % --------------------------- YOUR CODE HERE --------------------------
-% Train the sparse autoencoder and fill the following variables with 
+% Train the sparse autoencoder and fill the following variables with
 % the optimal parameters:
 
-optTheta =  zeros(2*hiddenSize*visibleSize+hiddenSize+visibleSize, 1);
-ZCAWhite =  zeros(visibleSize, visibleSize);
-meanPatch = zeros(visibleSize, 1);
-
+load '../linear_decoder_exercise/STL10Features.mat'
+ZCAWhite = eye(size(ZCAWhite, 1));  % TODO(zellyn): remove this
+meanPatch = zeros(size(meanPatch));
 
 % --------------------------------------------------------------------
 
@@ -61,13 +62,13 @@ displayColorNetwork( (W*ZCAWhite)');
 %% STEP 2a: Implement convolution
 %  Implement convolution in the function cnnConvolve in cnnConvolve.m
 
-% Note that we have to preprocess the images in the exact same way 
+% Note that we have to preprocess the images in the exact same way
 % we preprocessed the patches before we can obtain the feature activations.
 
-load stlTrainSubset.mat % loads numTrainImages, trainImages, trainLabels
+load '../data/stl10_matlab/stlTrainSubset.mat' % loads numTrainImages, trainImages, trainLabels
 
 %% Use only the first 8 images for testing
-convImages = trainImages(:, :, :, 1:8); 
+convImages = trainImages(:, :, :, 1:8);
 
 % NOTE: Implement cnnConvolve in cnnConvolve.m first!
 convolvedFeatures = cnnConvolve(patchDim, hiddenSize, convImages, W, b, ZCAWhite, meanPatch);
@@ -77,19 +78,23 @@ convolvedFeatures = cnnConvolve(patchDim, hiddenSize, convImages, W, b, ZCAWhite
 %  provided some code to compare the results of your convolution with
 %  activations from the sparse autoencoder
 
+rand("seed", 3);
+
 % For 1000 random points
-for i = 1:1000    
+for i = 1:1000
     featureNum = randi([1, hiddenSize]);
     imageNum = randi([1, 8]);
     imageRow = randi([1, imageDim - patchDim + 1]);
-    imageCol = randi([1, imageDim - patchDim + 1]);    
-   
+    imageCol = randi([1, imageDim - patchDim + 1]);
+
     patch = convImages(imageRow:imageRow + patchDim - 1, imageCol:imageCol + patchDim - 1, :, imageNum);
-    patch = patch(:);            
+    patch = patch(:);
     patch = patch - meanPatch;
     patch = ZCAWhite * patch;
-    
-    features = feedForwardAutoencoder(optTheta, hiddenSize, visibleSize, patch); 
+
+    W(featureNum, :) * patch
+
+    features = feedForwardAutoencoder(optTheta, hiddenSize, visibleSize, patch);
 
     if abs(features(featureNum, 1) - convolvedFeatures(featureNum, imageNum, imageRow, imageCol)) > 1e-9
         fprintf('Convolved feature does not match activation from autoencoder\n');
@@ -98,9 +103,9 @@ for i = 1:1000
         fprintf('Image Row         : %d\n', imageRow);
         fprintf('Image Column      : %d\n', imageCol);
         fprintf('Convolved feature : %0.5f\n', convolvedFeatures(featureNum, imageNum, imageRow, imageCol));
-        fprintf('Sparse AE feature : %0.5f\n', features(featureNum, 1));       
+        fprintf('Sparse AE feature : %0.5f\n', features(featureNum, 1));
         error('Convolved feature does not match activation from autoencoder');
-    end 
+    end
 end
 
 disp('Congratulations! Your convolution code passed the test.');
@@ -118,9 +123,9 @@ pooledFeatures = cnnPool(poolDim, convolvedFeatures);
 testMatrix = reshape(1:64, 8, 8);
 expectedMatrix = [mean(mean(testMatrix(1:4, 1:4))) mean(mean(testMatrix(1:4, 5:8))); ...
                   mean(mean(testMatrix(5:8, 1:4))) mean(mean(testMatrix(5:8, 5:8))); ];
-            
+
 testMatrix = reshape(testMatrix, 1, 1, 8, 8);
-        
+
 pooledFeatures = squeeze(cnnPool(4, testMatrix));
 
 if ~isequal(pooledFeatures, expectedMatrix)
@@ -147,8 +152,8 @@ end
 stepSize = 50;
 assert(mod(hiddenSize, stepSize) == 0, 'stepSize should divide hiddenSize');
 
-load stlTrainSubset.mat % loads numTrainImages, trainImages, trainLabels
-load stlTestSubset.mat  % loads numTestImages,  testImages,  testLabels
+load '../data/stl10_matlab/stlTrainSubset.mat' % loads numTrainImages, trainImages, trainLabels
+load '../data/stl10_matlab/stlTestSubset.mat'  % loads numTestImages,  testImages,  testLabels
 
 pooledFeaturesTrain = zeros(hiddenSize, numTrainImages, ...
     floor((imageDim - patchDim + 1) / poolDim), ...
@@ -160,27 +165,27 @@ pooledFeaturesTest = zeros(hiddenSize, numTestImages, ...
 tic();
 
 for convPart = 1:(hiddenSize / stepSize)
-    
+
     featureStart = (convPart - 1) * stepSize + 1;
     featureEnd = convPart * stepSize;
-    
-    fprintf('Step %d: features %d to %d\n', convPart, featureStart, featureEnd);  
+
+    fprintf('Step %d: features %d to %d\n', convPart, featureStart, featureEnd);
     Wt = W(featureStart:featureEnd, :);
-    bt = b(featureStart:featureEnd);    
-    
+    bt = b(featureStart:featureEnd);
+
     fprintf('Convolving and pooling train images\n');
     convolvedFeaturesThis = cnnConvolve(patchDim, stepSize, ...
         trainImages, Wt, bt, ZCAWhite, meanPatch);
     pooledFeaturesThis = cnnPool(poolDim, convolvedFeaturesThis);
-    pooledFeaturesTrain(featureStart:featureEnd, :, :, :) = pooledFeaturesThis;   
+    pooledFeaturesTrain(featureStart:featureEnd, :, :, :) = pooledFeaturesThis;
     toc();
     clear convolvedFeaturesThis pooledFeaturesThis;
-    
+
     fprintf('Convolving and pooling test images\n');
     convolvedFeaturesThis = cnnConvolve(patchDim, stepSize, ...
         testImages, Wt, bt, ZCAWhite, meanPatch);
     pooledFeaturesThis = cnnPool(poolDim, convolvedFeaturesThis);
-    pooledFeaturesTest(featureStart:featureEnd, :, :, :) = pooledFeaturesThis;   
+    pooledFeaturesTest(featureStart:featureEnd, :, :, :) = pooledFeaturesThis;
     toc();
 
     clear convolvedFeaturesThis pooledFeaturesThis;
